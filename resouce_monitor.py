41873e03f9a737b3288da8298f295adee3d8763d
Pablo Carvalho <111203477+pakilhas@users.noreply.github.com>
2023-11-17 17:47:24 -0300

    atualização

--- resouce_monitor.py
+++ resouce_monitor.py
@@ -3,10 +3,10 @@ import time
 
 def monitor_resources():
 	while True:
-		cpu_percent = psutil.cpu_percent(interval=1)
-		mem_percent = psutil.virtual_memory().percent
+		cpu_p = psutil.cpu_percent(interval=1)
+		mem_p = psutil.virtual_memory().percent
 
-		print(f"CPU USADA: {cpu_percent}% | Memoria Usada: {mem_percent}%")
+		print(f"CPU USADA: {cpu_p}% | Memoria Usada: {mem_p}%")
 		time.sleep(1)
 if __name__ == "__main__":
 	monitor_resources ()
